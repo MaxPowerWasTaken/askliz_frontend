ad79af1e3a119521e726b676e5ae4a292c52dba9
MaxAtMPC <max@maxpowerconsulting.com>
2024-11-25 22:51:57 -0600

    issue#6 - Make more frontend RAG pipeline options configurable from UI

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import litellm
 import pandas as pd
 import streamlit as st
 
-from lancedb.rerankers import CohereReranker, RRFReranker, LinearCombinationReranker
+from lancedb.rerankers import RRFReranker, CohereReranker, LinearCombinationReranker, CrossEncoderReranker, ColbertReranker
 
 from config import (LANCEDB_API_KEY,
                     COHERE_API_KEY, 
@@ -17,7 +17,8 @@ from generate_llm_response import generate_response
 def get_most_relevant_chunks(tbl: lancedb.table,
                    query: str, 
                    num_results_retrieved: int = N_RESULTS_RETRIEVED,
-                   num_results_to_llm: int = N_TOP_RERANKED_RESULTS_TO_LLM,
+                   final_rr = CohereReranker(model_name="rerank-english-v3.0", api_key=COHERE_API_KEY),
+                   #num_results_to_llm: int = N_TOP_RERANKED_RESULTS_TO_LLM,
                    )->pd.DataFrame:
     """Query the document database."""
 
@@ -28,25 +29,53 @@ def get_most_relevant_chunks(tbl: lancedb.table,
     prelim_results = (tbl.search(query, query_type="hybrid")
             .limit(num_results_retrieved)
             .rerank(reranker=prelim_rr)
-            )
+            ).limit(num_results_retrieved) # explicitly do not filter out any (lancedb cloud sometimes curiously does)
     df0 = prelim_results.to_pandas()
 
     # final reranked results (in _relevance_score order as determined by reranker)
-    final_rr = CohereReranker(model_name="rerank-english-v3.0", api_key=COHERE_API_KEY)
-    final_results = prelim_results.rerank(reranker=final_rr).limit(num_results_to_llm)
+    reranked_results = prelim_results.rerank(reranker=final_rr)
     
     # joining final reranked results with intermediate distance/bm25 scores
-    score_cols = ['_score', '_distance']
-    join_cols = ['chunk_idx']
-    results_df = final_results.to_pandas().merge(df0[score_cols+join_cols], on=join_cols, how="left")
-    assert results_df['_relevance_score'].is_monotonic_decreasing
+    scores = ['_score', '_distance']
+    key = ['chunk_idx']
+    results_df = df0[scores+key].merge(reranked_results.to_pandas(), on=key, how="left")
 
-    return results_df
+    return results_df.sort_values(by='_relevance_score', ascending=False).reset_index(drop=True)
 
+def get_selected_rr(selected_reranker):
+    match selected_reranker:
+        case "CohereReranker":
+            return CohereReranker(model_name="rerank-english-v3.0", api_key=COHERE_API_KEY)
+        case "LinearCombinationReranker":
+            return LinearCombinationReranker()  # default (vector-score) weight=0.7
+        case "CrossEncoderReranker":
+            return CrossEncoderReranker()
+        case "ColbertReranker":
+            return ColbertReranker()
+        case _:
+            raise ValueError(f"Unknown reranker type: {selected_reranker}")
+            
 def main():
     st.title("Ask Liz about the January 6 Select Committee's Findings")
 
     with st.sidebar:
+        # Input Number of Chunks Retrieved by 1st Stage; range: 5-15
+        num_results_retrieved = st.number_input(label="Number of Chunks Retrieved (1st Stage)",
+                                                min_value=5, max_value=15, value=N_RESULTS_RETRIEVED,
+                                                help="Number of document chunks retrieved from the document database (1st stage; before re-ranking)")
+
+        # Input Type of Reranker
+        selected_reranker = st.selectbox(label="Type of Reranker",
+                                         options=["CohereReranker", "LinearCombinationReranker", "CrossEncoderReranker", "ColbertReranker"],
+                                         index=0,
+                                         help="Type of reranker to use for re-ranking the retrieved document chunks")
+        final_rr = get_selected_rr(selected_reranker)
+
+        # Input Number of Top-Reranked Chunks to LLM; range: 1-5
+        num_results_to_llm = st.number_input(label="Number of Top-Reranked Chunks to LLM",
+                                             min_value=1, max_value=10, value=N_TOP_RERANKED_RESULTS_TO_LLM,
+                                             help="Number of top-ranked document chunks to send to LLM for final answer formulation")
+
         selected_llm_model = st.selectbox(label="Final LLM Model",
                                           options=SELECTED_LITELLM_MODEL_OPTIONS, 
                                           index=SELECTED_LITELLM_MODEL_OPTIONS.index(DEFAULT_FINAL_LLM_MODEL),
@@ -63,8 +92,15 @@ def main():
     
     if query:
         tbl = lancedb.connect(CLOUD_DB_URI, api_key=LANCEDB_API_KEY).open_table("document_chunks")        
-        retrieved_chunks_df = get_most_relevant_chunks(tbl, query)
-        final_response, final_prompt = generate_response(query, retrieved_chunks_df, llm_name=selected_llm_model)
+        retrieved_chunks_df = get_most_relevant_chunks(tbl, 
+                                                       query, 
+                                                       num_results_retrieved=num_results_retrieved,
+                                                       final_rr=final_rr,
+                                                       )
+        final_response, final_prompt = generate_response(query, 
+                                                         retrieved_chunks_df, 
+                                                         num_results_to_llm=num_results_to_llm,
+                                                         llm_name=selected_llm_model)
         
         if not show_steps:
             st.write(final_response)
--- generate_llm_response.py
+++ generate_llm_response.py
@@ -5,8 +5,9 @@ from typing import Tuple
 from google.generativeai import GenerativeModel
 def generate_response(
     query: str, 
-    context_passages: pd.DataFrame, 
-    llm_name: str,
+    context_passages: pd.DataFrame,
+    num_results_to_llm: int = 3,
+    llm_name: str = 'Gemini/gemini-1.5-pro-latest',
 ) -> Tuple[str, str]:
     """
     Generate a response using the provided LLM based on the query and retrieved context.
@@ -26,9 +27,11 @@ def generate_response(
         The generated response
     """
     # Prepare the context from retrieved passages
-    context_passages_list = context_passages['text'].tolist()
+    print(context_passages)
+    sorted_context_passages = context_passages.sort_values(by="_relevance_score", ascending=False)
+    context_chunks_for_llm = sorted_context_passages['text'].head(num_results_to_llm).tolist()
     context = "\n\n".join([f"Context {i+1}:\n{text_chunk}" 
-                          for i, text_chunk in enumerate(context_passages_list)])
+                          for i, text_chunk in enumerate(context_chunks_for_llm)])
     
     prompt = f"""Based on the following context, please answer the question. 
     Use only the information provided in the context. If you cannot answer the question 
